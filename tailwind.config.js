eeda74d990d132c6d2f2b1f3f29fab2b0c4ea51e
Mohammdreza.TH <mth.cb1446@gmail.com>
2023-04-21 19:07:18 +0430

    feat:some parts of homePage

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,25 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-    },
+    },screens: {
+      'sm': '200px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }, fontFamily: {
+      'nunito': ['nunito', 'sans-serif'],
+      'MyFont': ['"My Font"', 'serif'] // Ensure fonts with spaces have " " surrounding it.
+    }
   },
   plugins: [],
 }
