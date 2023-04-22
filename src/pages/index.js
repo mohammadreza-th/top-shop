413deec37cf42b006b06b5caf20fff6bb7a8bdf2
Mohammdreza.TH <mth.cb1446@gmail.com>
2023-04-22 05:24:50 +0430

    fix: styling and some media query for homePage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,40 @@
-import img from  "../../public/images/1.png"
+import Link from "next/link";
+
 const Home = () => {
   return (
-    <>
-    <div className="h-screen md:bg-white sm:bg-red-500">
-      <div className="absolute left-14 top-14 p-16 rounded-full bg-white"></div>
-      <h2 className="absolute top-44 left-14 text-white text-7xl">Food for Evrey one</h2>
+    <div className="relative h-screen pt-10 pb-10 bg-red-400 ">
+      <div className="h-[100%] w-[90%] m-auto py-16 flex flex-col justify-between  ">
+        <img
+          className="rounded-full w-[12vh] h-[12vh] xl:w-[24vh] xl:h-[24vh] bg-white p-5"
+          src="/images/logo.png"
+          width={"100%"}
+          height={"100%"}
+          alt=""
+        />
+        <h2 className="font-bold animate-pulse text-[10vh]/[10vh] text-white">
+          Food for
+          <br /> Evrey one
+        </h2>
+        <div></div>
+        <div></div>
+        <div className="absolute left-1/2 bottom-[15%] xl:bottom-[26%] z-10 h-1/6 w-full -translate-x-2/4	 bg-gradient-to-t from-red-400/100 to-red-400/0 via-red-400/80"></div>
+        <img
+          className="animate-incommingL w-[50%] xl:max-w-[300px] xl:left-[60%] absolute bottom-[16%] xl:bottom-[27%] "
+          src="/images/homePic.png"
+          alt="homePic"
+        />
+        <img
+          className="animate-incommingR w-[30%] xl:max-w-[200px] absolute bottom-[16%] xl:bottom-[27%] rotate-12 right-32"
+          src="/images/homePic2.png"
+          alt="homePic"
+        />
+        <Link href={"/login"}>
+          <button className="w-full grow  pb-5 bg-white rounded-r-full rounded-l-full text-red-400 text-[5vh] font-['font4']">
+            get started
+          </button>
+        </Link>
+      </div>
     </div>
-    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,53 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      keyframes: {
+        incommingL: {
+          "0%": { transform: "translatex(-20rem)", opacity: "0" },
+          "100%": {},
+        },
+        incommingR: {
+          "0%": { transform: "translateX(10rem)", opacity: "0" },
+          "100%": { transform: "rotate(12deg)" },
+        },
       },
-    },screens: {
-      'sm': '200px',
+      animation: {
+        incommingL: "incommingL 1s ease-in-out",
+
+        incommingR: "incommingR 1s ease-in-out",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+    screens: {
+      sm: "640px",
       // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
+      md: "768px",
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
+      lg: "1024px",
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
+      xl: "1280px",
       // => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
+      "2xl": "1536px",
       // => @media (min-width: 1536px) { ... }
-    }, fontFamily: {
-      'nunito': ['nunito', 'sans-serif'],
-      'MyFont': ['"My Font"', 'serif'] // Ensure fonts with spaces have " " surrounding it.
-    }
+    },
+    fontFamily: {
+      nunito: ["nunito", "sans-serif"],
+      MyFont: ['"My Font"', "serif"], // Ensure fonts with spaces have " " surrounding it.
+    },
   },
   plugins: [],
-}
+};
