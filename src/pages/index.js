307310065d35a5f868f90d491516f979b52fa76d
Mohammdreza.TH <mth.cb1446@gmail.com>
2023-04-22 18:19:34 +0430

    fix: finishing media quary styling in homePage

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ const Home = () => {
     <div className="relative h-screen pt-10 pb-10 bg-red-400 ">
       <div className="h-[100%] w-[90%] m-auto py-16 flex flex-col justify-between  ">
         <img
-          className="rounded-full w-[12vh] h-[12vh] xl:w-[24vh] xl:h-[24vh] bg-white p-5"
+          className="rounded-full w-[12vw] h-[12vw] vs:w-[12vh] vs:h-[12vh] sm:w-[12vh] sm:h-[12vh] bg-white p-5"
           src="/images/logo.png"
           width={"100%"}
           height={"100%"}
@@ -17,19 +17,19 @@ const Home = () => {
         </h2>
         <div></div>
         <div></div>
-        <div className="absolute left-1/2 bottom-[15%] xl:bottom-[26%] z-10 h-1/6 w-full -translate-x-2/4	 bg-gradient-to-t from-red-400/100 to-red-400/0 via-red-400/80"></div>
+        <div className="absolute left-1/2 bottom-[26%] z-10 h-1/6 w-full -translate-x-2/4	 bg-gradient-to-t from-red-400/100 to-red-400/0 via-red-400/80"></div>
         <img
-          className="animate-incommingL w-[50%] xl:max-w-[300px] xl:left-[60%] absolute bottom-[16%] xl:bottom-[27%] "
+          className="animate-incommingL w-[50%] max-w-[300px] right-[10%] absolute bottom-[27%] "
           src="/images/homePic.png"
           alt="homePic"
         />
         <img
-          className="animate-incommingR w-[30%] xl:max-w-[200px] absolute bottom-[16%] xl:bottom-[27%] rotate-12 right-32"
+          className="animate-incommingR w-[30%] max-w-[200px] absolute  bottom-[27%] rotate-12 right-[8%]"
           src="/images/homePic2.png"
           alt="homePic"
         />
         <Link href={"/login"}>
-          <button className="w-full grow  pb-5 bg-white rounded-r-full rounded-l-full text-red-400 text-[5vh] font-['font4']">
+          <button className="w-full p-auto min-h-[50px] py-[3px] max-h-[70px] bg-white rounded-r-full rounded-l-full text-red-400 text-[40px] vs:text-[28px] font-['font4']">
             get started
           </button>
         </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -29,19 +29,21 @@ module.exports = {
       },
     },
     screens: {
-      sm: "640px",
-      // => @media (min-width: 640px) { ... }
+      'vs': {'min': '100px', 'max': '400px'},
+      // => @media (min-width: 640px and max-width: 767px) { ... }
+      'sm': {'min': '400px', 'max': '767px'},
+      // => @media (min-width: 640px and max-width: 767px) { ... }
 
-      md: "768px",
-      // => @media (min-width: 768px) { ... }
+      'md': {'min': '768px', 'max': '1023px'},
+      // => @media (min-width: 768px and max-width: 1023px) { ... }
 
-      lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
+      'lg': {'min': '1024px', 'max': '1279px'},
+      // => @media (min-width: 1024px and max-width: 1279px) { ... }
 
-      xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
+      'xl': {'min': '1280px', 'max': '1535px'},
+      // => @media (min-width: 1280px and max-width: 1535px) { ... }
 
-      "2xl": "1536px",
+      '2xl': {'min': '1536px'},
       // => @media (min-width: 1536px) { ... }
     },
     fontFamily: {
