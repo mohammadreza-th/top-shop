5d83663b6b23dccb999d13720a24da7d93a3092f
Mohammdreza.TH <mth.cb1446@gmail.com>
2023-07-19 14:56:01 +0330

    trying to solve deploy bugs

--- src/components/Cards/ResultCard.jsx
+++ src/components/Cards/ResultCard.jsx
@@ -1,7 +1,8 @@
+import Image from "next/image";
 const ResultCard = ({ src, title, price }) => {
   return (
     <div className="relative flex flex-col items-center mt-14 mx-auto rounded-2xl bg-white h-[110px] w-[100px] ">
-      <img
+      <Image
         className="absolute left-[50%] -translate-y-1/2 -translate-x-1/2 rounded-full
          bg-gradient-to-r from-red-300 to-red-50 w-[75px] aspect-square p-1"
         src={src ? src[0] : `/images/${Math.round(Math.random() * 13)}.png`}
--- src/components/NoResult/NoResult.jsx
+++ src/components/NoResult/NoResult.jsx
@@ -8,7 +8,9 @@ const NoResult = () => {
       <div class=" flex h-screen bg-white">
         <div class="m-auto flex-col justify-center items-center">
           <div className="flex justify-center">
-            <Image src={historyImage} />
+            <Image 
+            alt=" "
+            src={historyImage} />
           </div>
           <h3 className="w-52  font-semibold text-3xl leading-8 text-center my-4">No history yet</h3>
           <p className="w-56  font-normal text-base leading-5 text-center">Hit the orange button down below to Create an order</p>
